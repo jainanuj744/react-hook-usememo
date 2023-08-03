8054e5afe76375b56700e83c3ee517165dc32a52
Anuj Jain <jainanuj744@gmail.com>
2023-08-03 16:09:26 +0530

    that's how usememo hooks works

---
+++ src/Factorial.jsx
@@ -0,0 +1,32 @@
+import { useMemo, useState } from "react";
+
+export default function Factorial() {
+  let [num, setNum] = useState(1);
+  let [inc, setInc] = useState(0);
+
+  let onChange = (e) => {
+    setNum(e.target.value);
+  };
+
+  let onInc = () => {
+    setInc(inc + 1);
+  };
+  
+//   that's how usememo works
+  const fxn = useMemo(() => factorial(num),[num]);
+
+  return (
+    <div>
+      Factorial of
+      <input value={num} onChange={onChange} type="number" />
+      <br />
+      is {fxn}
+      <br />
+      <button onClick={onInc}>Use Memo</button>
+    </div>
+  );
+}
+function factorial(a) {
+  console.log("Factorial fxn called");
+  return a <= 0 ? 1 : a * factorial(a - 1);
+}
